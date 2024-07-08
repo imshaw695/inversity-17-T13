1c91c4c39407fc81f99712cf99ee48f7416502a4
imshaw695 <ivan.shaw695@gmail.com>
2024-07-08 13:33:48 +0100

    footer moved

--- front_end/src/views/DashboardView.vue
+++ front_end/src/views/DashboardView.vue
@@ -16,38 +16,6 @@
       <iframe src="https://ourworldindata.org/grapher/wind-energy-consumption-vs-installed-wind-energy-capacity?tab=chart" loading="lazy" style="width: 100%; height: 600px; border: 0px none;" allow="web-share; clipboard-write"></iframe>
       </div>
     </div>
-    <footer
-      class="bg-white rounded-lg shadow m-4 dark:bg-gray-800 bottom-0 w-full"
-      style="z-index: 9999"
-    >
-      <MessageDisplay v-bind:messages="messages" />
-      <div
-        class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between"
-      >
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
-          >© 2024
-          <a href="https://flowbite.com/" class="hover:underline"
-            >Crown Estate</a
-          >. All Rights Reserved.
-        </span>
-        <ul
-          class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0"
-        >
-          <li>
-            <a href="#" class="hover:underline me-4 md:me-6">About</a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </footer>
   </main>
 </template>
 
